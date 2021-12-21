b25800884d4643c5dbbea224379cb4f18610bb21
Fethan Us <usfethan@gmail.com>
2021-12-20 20:34:48 -0500

    added data validation

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
-const apiRouter = require("./routes/apiRouter");
-const htmlrouter = require("./routes/htmlRouter");
-
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -14,8 +11,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use(htmlrouter);
-app.use(apiRouter);
+const { notes } = require("./db/db.json");
+const { off } = require("process");
+
+function createNewNote (body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, "./db/db.json"),
+        JSON.stringify({ notes: notesArray}, null, 2)
+    );
+    return note;
+};
+
+function validateNote (note) {
+    if(!note.title || typeof note.title !== "string") {
+        return false;
+    } 
+    if (!note.text || typeof note.text !== "string") {
+        return true;
+    };
+
+    app.get("/api/notes", (req, res) => {
+        res.json(notes);
+    });
+    
+  
+}
+
 
 app.listen(PORT, () =>
     console.log(`Server listening at http://localhost:${PORT}`)
